e735e4e27554c76244eab2be3709eed3034b8c14
Thomas Brekke <tcbrekke@gmail.com>
2018-04-10 10:31:10 -0700

    changed code to better handle duplicates

--- plotbot_tcb.py
+++ plotbot_tcb.py
@@ -13,11 +13,6 @@ access_token = os.environ.get('access_token')
 access_token_secret = os.environ.get('access_token_secret')
 consumer_key = os.environ.get('consumer_key')
 consumer_secret = os.environ.get('consumer_secret')
-
-#from config import (consumer_key,
-#                   consumer_secret,
-#                    access_token,
-#                    access_token_secret)  
                     
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
@@ -29,6 +24,9 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 sns.set()
 
+checked_list = []
+most_recent_tweet = 0
+
 for status in tweepy.Cursor(api.search, q="Analyze AND @tcbbot OR analyze AND @tcbbot", tweet_mode='extended').items(5):
     tweet = status._json
     
@@ -36,15 +34,17 @@ for status in tweepy.Cursor(api.search, q="Analyze AND @tcbbot OR analyze AND @t
     split_tweet = stripped_tweet.split(' ')
     target_user = split_tweet[-1].strip('@')
     
+    
     mentioner = tweet['user']['screen_name'].replace("'", "")
     
+    
     try:
-        compound_list = []
-
-        if mentioner in set(checked_list):
-            api.update_status(f"Sorry @{mentioner}, someone has already plotted that - that plot is here:")
-
+        if target_user in set(checked_list):
+            if tweet['id'] > most_recent_tweet:
+                api.update_status(f"Sorry @{mentioner}, someone has already plotted that.")
+                most_recent_tweet = tweet['id']
         else:
+            compound_list = []
             for status in tweepy.Cursor(api.user_timeline, id=target_user, tweet_mode='extended').items(500):
                 target_user_tweet = status._json
                 results = analyzer.polarity_scores(target_user_tweet["full_text"])
@@ -55,20 +55,23 @@ for status in tweepy.Cursor(api.search, q="Analyze AND @tcbbot OR analyze AND @t
 
             plot_title_fonts = {'family':'sans-serif', 'size':18}
 
-            comp.set_title(f"Sentiment of @{target_user}'s Past 500 Tweets", fontdict=plot_title_fonts)
+            x_axis = len(compound_list)
+        
+            comp.set_title(f"Sentiment of @{target_user}'s Past {x_axis} Tweets", fontdict=plot_title_fonts)
             comp.set_xlabel('Tweets (from oldest to newest)', fontdict=plot_title_fonts)
             comp.set_ylabel('Compound Sentiment (VADER)', fontdict=plot_title_fonts)
-            comp.set_xlim(0,len(compound_list))
+            comp.set_xlim(0,x_axis)
             comp.set_ylim(-1,1)
 
-            plot_filename = f"{target_user}_500_sentiment_plot.png"
+            plot_filename = f"{target_user}_{x_axis}_sentiment_plot.png"
             plot_path = os.path.join("saved-figs", plot_filename)
-            comp.plot(range(len(compound_list)), compound_list, marker='o', color='red', mec='black', alpha=0.5)
+            comp.plot(range(x_axis), compound_list, marker='o', color='red', mec='black', alpha=0.5)
             plt.savefig(plot_path, dpi=300)
 
             api.update_with_media(f"{plot_path}",
-                            f"Here is your plot of the compound sentiment for @{target_user}'s past 500 tweets:")
-    except:
+                            f"Here is your plot of the compound sentiment for @{target_user}'s past {x_axis} tweets:")
+            checked_list.append(target_user)
+    except Exception:
         continue
 
 time.sleep(300)
